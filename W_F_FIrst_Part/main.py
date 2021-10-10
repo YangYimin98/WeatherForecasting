e5f37319b6c65db3d0815cc1acfc543f349180f9
Tommy980510 <yimin.yang0510@gmail.com>
2021-10-10 15:47:39 +0200

    a

--- W_F_FIrst_Part/main.py
+++ W_F_FIrst_Part/main.py
@@ -20,17 +20,25 @@ from fbprophet import Prophet
 # data.to_csv('test.csv', encoding='utf-8')
 # data = pd.read_csv('test.csv')
 # test = pd.read_csv('csv/step1_Xp_1_0_.csv')
-test = sio.loadmat('data.mat')
-# print(data.keys())
-# print(data.shape)
-
-# print(test['X'].shape)
-x = test['X'][:, 0, 0]
-# print(x.shape)
-ds = pd.date_range('20000101', periods=70128)
-df = pd.DataFrame(data=x, index=ds, columns=['y'])
-df['ds'] = pd.date_range('20000101', periods=70128)
+# test = sio.loadmat('data.mat')
+# # print(data.keys())
+# # print(data.shape)
+#
+# # print(test['X'].shape)
+# x = test['X'][:, 0, 0]
+# # print(x.shape)
+# ds = pd.date_range('20000101', periods=70128)
+# df = pd.DataFrame(data=x, index=ds, columns=['y'])
+# df['ds'] = pd.date_range('20000101', periods=70128)
 
 # print(df)
 
 # df.to_csv('test.csv', index=False, header=False)
+import pickle
+
+# loading dataset
+with open('/Users/yangyimin/PycharmProjects/WeatherForecasting/W_F_FIrst_Part/dataset.pkl', 'rb') as f:
+    dataset = pickle.load(f)
+print(type(dataset))
+print(dataset.keys())
+print(dataset['train'].shape)
